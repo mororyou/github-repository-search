0168eb9a99b39e5c133e5254879360abca74f30b
R.Moroizumi <m.ryousuke0401@icloud.com>
2025-04-17 05:12:55 +0900

    ✨ Feat: リポジトリ検索機能を無限スクロールに対応させ、キーワードによるフィルタリングを追加

--- src/queries/repository/index.ts
+++ src/queries/repository/index.ts
@@ -1,14 +1,32 @@
 import { getRepositories } from '@/server/repositories/repository';
-import { atomWithMutation } from 'jotai-tanstack-query';
+import { atom } from 'jotai';
+import { atomWithInfiniteQuery } from 'jotai-tanstack-query';
 
-export const repositoriesAtom = atomWithMutation(() => ({
-  mutationFn: async ({
-    repositoryName,
-    page,
-  }: {
-    repositoryName: string;
-    page: number;
-  }) => {
-    return await getRepositories({ repositoryName, page });
-  },
-}));
+export const keywordAtom = atom<string>('');
+
+export const perPage = 3;
+
+export const repositoriesAtom = atomWithInfiniteQuery((get) => {
+  const keyword = get(keywordAtom);
+  return {
+    queryKey: ['repositories', keyword],
+    queryFn: async ({ pageParam = 1 }) => {
+      return await getRepositories({
+        repositoryName: keyword,
+        page: pageParam as number,
+      });
+    },
+    getNextPageParam: (lastPage, allPages, lastPageParam) => {
+      if (
+        lastPage.data?.total_count &&
+        allPages.length * perPage >= lastPage.data?.total_count
+      ) {
+        return undefined;
+      }
+      return (lastPageParam as number) + 1;
+    },
+    initialPageParam: 1,
+    enabled: !!keyword, // キーワードの長さが0より大きい場合のみ有効化
+    staleTime: 0, // キャッシュを無効化
+  };
+});
