782e5cbc026e781c3f83e5308cf7e9f70453dd99
R.Moroizumi <m.ryousuke0401@icloud.com>
2025-04-17 15:10:07 +0900

    :white_check_mark: Test: zSearchRepositoryResultItemSchema,zSearchRepositoryResultsSchema のテスト実装

--- src/schemas/validations/__specs__/search.spec.ts
+++ src/schemas/validations/__specs__/search.spec.ts
@@ -1,7 +1,11 @@
 import { describe, it } from 'vitest';
-import { zSearchRepositorySchema } from '../search';
+import {
+  zSearchRepositoryResultItemSchema,
+  zSearchRepositoryResultsSchema,
+  zSearchRepositorySchema,
+} from '../search';
 
-describe('zSearchRepositorySchema', () => {
+describe('zSearchRepositorySchema validation tests', () => {
   it('半角英数字で入力した場合、正常に通る', ({ expect }) => {
     const result = zSearchRepositorySchema.parse({
       repositoryName: 'test1234',
@@ -38,6 +42,180 @@ describe('zSearchRepositorySchema', () => {
 
     expect(result.error?.errors[0].message).toBe(
       '半角英数字で入力してください',
+    );
+  });
+
+  it('nullの場合、エラーを返す', ({ expect }) => {
+    expect(() => zSearchRepositorySchema.parse(null)).toThrowError(
+      'レポジトリ名を入力してください',
+    );
+  });
+
+  it('undefinedの場合、エラーを返す', ({ expect }) => {
+    expect(() => zSearchRepositorySchema.parse(undefined)).toThrowError(
+      'レポジトリ名を入力してください',
+    );
+  });
+});
+
+describe('zSearchRepositoryResultItemSchema validation tests', () => {
+  it('正常な値の場合', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse({
+        id: 1,
+        name: 'test',
+        full_name: 'test/test',
+        owner: {
+          avatar_url: 'https://example.com/avatar.png',
+          login: 'test',
+        },
+        html_url: 'https://example.com/test',
+        description: 'test',
+        stargazers_count: 1,
+        forks_count: 1,
+        topics: ['test', 'test2'],
+        language: 'test',
+      }),
+    ).not.toThrowError();
+  });
+
+  it('idが数値でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse({
+        id: 'test',
+      }),
+    ).toThrowError('idは数値で入力してください');
+  });
+
+  it('nameが文字列でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse({
+        name: 123,
+      }),
+    ).toThrowError('nameは文字列で入力してください');
+  });
+
+  it('full_nameが文字列でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse({
+        full_name: 123,
+      }),
+    ).toThrowError('full_nameは文字列で入力してください');
+  });
+
+  it('descriptionが文字列でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse({
+        description: 123,
+      }),
+    ).toThrowError('descriptionは文字列で入力してください');
+  });
+
+  it('stargazers_countが数値でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse({
+        stargazers_count: 'test',
+      }),
+    ).toThrowError('stargazers_countは数値で入力してください');
+  });
+
+  it('forks_countが数値でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse({
+        forks_count: 'test',
+      }),
+    ).toThrowError('forks_countは数値で入力してください');
+  });
+
+  it('topicsが配列でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse({
+        topics: 'test',
+      }),
+    ).toThrowError('topicsは配列で入力してください');
+  });
+
+  it('languageが文字列でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse({
+        language: 123,
+      }),
+    ).toThrowError('languageは文字列で入力してください');
+  });
+
+  it('undefinedの場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultItemSchema.parse(undefined),
+    ).toThrowError('Null, Undefinedは許可されておりません');
+  });
+});
+
+describe('zSearchRepositoryResultsSchema validation tests', () => {
+  it('正常な値の場合', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultsSchema.parse({
+        total_count: 1,
+        items: [
+          {
+            id: 1,
+            name: 'test',
+            full_name: 'test/test',
+            owner: {
+              avatar_url: 'https://example.com/avatar.png',
+              login: 'test',
+            },
+            html_url: 'https://example.com/test',
+            description: 'test',
+            stargazers_count: 1,
+            forks_count: 1,
+            topics: ['test', 'test2'],
+            language: 'test',
+          },
+        ],
+        incomplete_results: false,
+      }),
+    ).not.toThrowError();
+  });
+
+  it('total_countが数値でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultsSchema.parse({
+        total_count: 'test',
+        items: [],
+        incomplete_results: false,
+      }),
+    ).toThrowError('total_countは数値で入力してください');
+  });
+
+  it('itemsが配列でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultsSchema.parse({
+        total_count: 1,
+        items: 'test',
+        incomplete_results: false,
+      }),
+    ).toThrowError('itemsは配列で入力してください');
+  });
+
+  it('incomplete_resultsが真偽値でない場合、エラーを返す', ({ expect }) => {
+    expect(() =>
+      zSearchRepositoryResultsSchema.parse({
+        total_count: 1,
+        items: [],
+        incomplete_results: 'test',
+      }),
+    ).toThrowError('incomplete_resultsは真偽値で入力してください');
+  });
+
+  it('Nullの場合、エラーを返す', ({ expect }) => {
+    expect(() => zSearchRepositoryResultsSchema.parse(null)).toThrowError(
+      'Null, Undefinedは許可されておりません',
+    );
+  });
+
+  it('undefinedの場合、エラーを返す', ({ expect }) => {
+    expect(() => zSearchRepositoryResultsSchema.parse(undefined)).toThrowError(
+      'Null, Undefinedは許可されておりません',
     );
   });
 });
